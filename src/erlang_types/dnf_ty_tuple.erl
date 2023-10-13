4a6d4c57bfb481009657e20b8e28737b71f3ca0a
Albert Schimpf <git@schimpf.link>
2023-10-13 13:44:49 +0200

    generic dnf_var_int dnf_var_predef

--- src/erlang_types/dnf_ty_tuple.erl
+++ src/erlang_types/dnf_ty_tuple.erl
@@ -3,56 +3,41 @@
 -define(P, {bdd_bool, ty_tuple}).
 -define(F(Z), fun() -> Z end).
 
-
 -export([equal/2, compare/2]).
-
-%%
 -export([empty/0, any/0, union/2, intersect/2, diff/2, negate/1]).
 -export([is_empty/1, is_any/1, normalize/6, substitute/4]).
-
 -export([tuple/1, all_variables/1, has_ref/2, transform/2]).
 
--type dnf_tuple() :: term().
--type ty_tuple() :: dnf_tuple(). % ty_tuple:type()
--type dnf_ty_tuple() :: term().
-
--spec tuple(ty_tuple()) -> dnf_ty_tuple().
 tuple(TyTuple) -> gen_bdd:element(?P, TyTuple).
 
-% ==
-% type interface
-% ==
 empty() -> gen_bdd:empty(?P).
 any() -> gen_bdd:any(?P).
-
 all_variables(TyBDD) -> gen_bdd:all_variables(?P, TyBDD).
-
 substitute(MkTy, TyBDD, Map, Memo) -> gen_bdd:substitute(?P, MkTy, TyBDD, Map, Memo).
-
 union(B1, B2) -> gen_bdd:union(?P, B1, B2).
 intersect(B1, B2) -> gen_bdd:intersect(?P, B1, B2).
 diff(B1, B2) -> gen_bdd:diff(?P, B1, B2).
 negate(B1) -> gen_bdd:negate(?P, B1).
-
 is_any(B) -> gen_bdd:is_any(?P, B).
 has_ref(Ty, Ref) -> gen_bdd:has_ref(?P, Ty, Ref).
-
-% ==
-% basic interface
-% ==
-
 equal(B1, B2) -> gen_bdd:equal(?P, B1, B2).
 compare(B1, B2) -> gen_bdd:compare(?P, B1, B2).
+transform(Ty, Ops) -> gen_bdd:transform(?P, Ty, Ops).
 
 is_empty(TyBDD) ->
   gen_bdd:dnf(?P, TyBDD, {fun is_empty_coclause/3, fun gen_bdd:is_empty_union/2}).
 
-is_empty_coclause(_Pos, _Neg, 0) -> true;
-is_empty_coclause([], _Neg, 1) -> false; % TODO check correctness of this
-is_empty_coclause(Pos, Neg, 1) ->
-  % invariant: Pos is single tuple (simplification step required)
-  BigS = ty_tuple:big_intersect(Pos),
-  phi(ty_tuple:components(BigS), Neg).
+is_empty_coclause(Pos, Neg, T) ->
+  case bdd_bool:empty() of
+    T -> true;
+    _ -> case Pos of
+           % TODO check correctness of this
+           [] -> false;
+           _ ->
+             BigS = ty_tuple:big_intersect(Pos),
+             phi(ty_tuple:components(BigS), Neg)
+         end
+  end.
 
 phi(BigS, []) ->
   lists:foldl(fun(S, Res) -> Res orelse ty_rec:is_empty(S) end, false, BigS);
@@ -122,18 +107,6 @@ phi_norm(Size, BigS, [Ty | N], Fixed, M) ->
 
 
   constraint_set:join(
-    ?F(lists:foldl(fun(S, Res) -> constraint_set:join(?F(Res), ?F(ty_rec:normalize(S, Fixed, M))) end, [], BigS))
-    ,
+    ?F(lists:foldl(fun(S, Res) -> constraint_set:join(?F(Res), ?F(ty_rec:normalize(S, Fixed, M))) end, [], BigS)),
     ?F(lists:foldl(Solve, [[]], lists:zip(lists:seq(1, length(ty_tuple:components(Ty))), lists:zip(BigS, ty_tuple:components(Ty)))))
   ).
-
-
-transform({terminal, 0}, #{empty := F}) -> F();
-transform({terminal, 1}, #{any_tuple := F}) -> F();
-transform({node, Tuple, PositiveEdge, NegativeEdge}, Ops = #{negate := N, union := U, intersect := I} ) ->
-  NF = ty_tuple:transform(Tuple, Ops),
-
-  U([
-    I([NF, transform(PositiveEdge, Ops)]),
-    I([N(NF), transform(NegativeEdge, Ops)])
-  ]).
--- src/erlang_types/dnf_var_int.erl
+++ src/erlang_types/dnf_var_int.erl
@@ -2,73 +2,36 @@
 
 -define(P, {ty_interval, ty_variable}).
 
-
 -export([equal/2, compare/2]).
-
-
 -export([empty/0, any/0, union/2, intersect/2, diff/2, negate/1]).
 -export([is_empty/1, is_any/1, normalize/3, substitute/4]).
-
 -export([var/1, int/1,  all_variables/1, transform/2]).
 
--type interval() :: term(). % interval:type()
--type variable() :: term(). % variable:type()
--type dnf_var_int() :: term().
-
--spec int(interval()) -> dnf_var_int().
 int(Interval) -> gen_bdd:terminal(?P, Interval).
-
--spec var(variable()) -> dnf_var_int().
 var(Var) -> gen_bdd:element(?P, Var).
 
-% ==
-% type interface
-% ==
+% generic
 empty() -> gen_bdd:empty(?P).
 any() -> gen_bdd:any(?P).
-
 union(B1, B2) -> gen_bdd:union(?P, B1, B2).
 intersect(B1, B2) -> gen_bdd:intersect(?P, B1, B2).
 diff(B1, B2) -> gen_bdd:diff(?P, B1, B2).
 negate(B1) -> gen_bdd:negate(?P, B1).
-
 is_any(B) -> gen_bdd:is_any(?P, B).
-
-% ==
-% basic interface
-% ==
 equal(B1, B2) -> gen_bdd:equal(?P, B1, B2).
 compare(B1, B2) -> gen_bdd:compare(?P, B1, B2).
+substitute(MkTy, T, M, Memo) -> gen_bdd:substitute(?P, MkTy, T, M, Memo).
+all_variables(TyBDD) -> gen_bdd:all_variables(?P, TyBDD).
+transform(Ty, Ops) -> gen_bdd:transform(?P, Ty, Ops).
+
+% partially generic
 is_empty(TyBDD) -> gen_bdd:dnf(?P, TyBDD, {fun is_empty_coclause/3, fun gen_bdd:is_empty_union/2}).
 is_empty_coclause(_Pos, _Neg, T) -> ty_interval:is_empty(T).
 
-% ==
-% Emptiness for variable interval DNFs
-% ==
-normalize(Ty, Fixed, M) -> normalize(Ty, [], [], Fixed, M).
-
-normalize({terminal, 0}, _, _, _, _) -> [[]]; % satisfiable
-normalize({terminal, Atom}, PVar, NVar, Fixed, M) ->
-  ty_interval:normalize(Atom, PVar, NVar, Fixed, M);
-normalize({node, Variable, PositiveEdge, NegativeEdge}, PVar, NVar, Fixed, M) ->
-  constraint_set:merge_and_meet(
-    normalize(PositiveEdge, [Variable | PVar], NVar, Fixed, M),
-    normalize(NegativeEdge, PVar, [Variable | NVar], Fixed, M)
-  ).
-
-substitute(MkTy, T, M, Memo) -> gen_bdd:substitute(?P, MkTy, T, M, Memo).
-all_variables(TyBDD) -> gen_bdd:all_variables(?P, TyBDD).
-
-transform({terminal, 0}, #{empty := E}) -> E();
-transform({terminal, Int}, Ops) ->
-  ty_interval:transform(Int, Ops);
-transform({node, Variable, PositiveEdge, NegativeEdge},
-    Ops = #{negate := Negate, var := ToVar, union := Union, intersect := Intersect}) ->
-  AstVar = ToVar(Variable),
-  Union([
-    Intersect([AstVar, transform(PositiveEdge, Ops)]),
-    Intersect([Negate(AstVar), transform(NegativeEdge, Ops)])
-  ]).
+normalize(Ty, Fixed, M) -> gen_bdd:dnf(?P, Ty, {
+  fun(Pos, Neg, Atom) -> ty_interval:normalize(Atom, Pos, Neg, Fixed, M) end,
+  fun constraint_set:meet/2
+}).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -122,4 +85,3 @@ compact_ints_test() ->
   ok.
 
 -endif.
-
