effc179d403332c47a2ebb4899fa3242942a603c
Albert Schimpf <git@schimpf.link>
2023-10-13 13:44:49 +0200

    generic dnf_var_int dnf_var_predef

--- src/erlang_types/dnf_var_predef.erl
+++ src/erlang_types/dnf_var_predef.erl
@@ -2,69 +2,32 @@
 
 -define(P, {ty_predef, ty_variable}).
 
-
 -export([equal/2, compare/2]).
-
-
 -export([empty/0, any/0, union/2, intersect/2, diff/2, negate/1]).
 -export([is_empty/1, is_any/1, normalize/3, substitute/4]).
-
 -export([var/1, predef/1,  all_variables/1, transform/2]).
 
--type variable() :: term(). % variable:type()
--type dnf_var_int() :: term().
-
+% generic
 predef(Predef) -> gen_bdd:terminal(?P, Predef).
-
--spec var(variable()) -> dnf_var_int().
 var(Var) -> gen_bdd:element(?P, Var).
-
-% ==
-% type interface
-% ==
 empty() -> gen_bdd:empty(?P).
 any() -> gen_bdd:any(?P).
-
 union(B1, B2) -> gen_bdd:union(?P, B1, B2).
 intersect(B1, B2) -> gen_bdd:intersect(?P, B1, B2).
 diff(B1, B2) -> gen_bdd:diff(?P, B1, B2).
 negate(B1) -> gen_bdd:negate(?P, B1).
-
 is_any(B) -> gen_bdd:is_any(?P, B).
+equal(B1, B2) -> gen_bdd:equal(?P, B1, B2).
+compare(B1, B2) -> gen_bdd:compare(?P, B1, B2).
+substitute(MkTy, T, M, _) -> gen_bdd:substitute(?P, MkTy, T, M, sets:new()).
+all_variables(TyBDD) -> gen_bdd:all_variables(?P, TyBDD).
+transform(Ty, Ops) -> gen_bdd:transform(?P, Ty, Ops).
+
+% partially generic
 is_empty(TyBDD) -> gen_bdd:dnf(?P, TyBDD, {fun is_empty_coclause/3, fun gen_bdd:is_empty_union/2}).
 is_empty_coclause(_Pos, _Neg, T) -> ty_predef:is_empty(T).
 
-
-% ==
-% basic interface
-% ==
-
-equal(B1, B2) -> gen_bdd:equal(?P, B1, B2).
-compare(B1, B2) -> gen_bdd:compare(?P, B1, B2).
-
-substitute(MkTy, T, M, _) -> gen_bdd:substitute(?P, MkTy, T, M, sets:new()).
-
-
-normalize(Ty, Fixed, M) -> normalize(Ty, [], [], Fixed, M).
-
-normalize({terminal, 0}, _, _, _, _) -> [[]]; % satisfiable
-normalize({terminal, Predef}, PVar, NVar, Fixed, M) ->
-  ty_predef:normalize(Predef, PVar, NVar, Fixed, M);
-normalize({node, Variable, PositiveEdge, NegativeEdge}, PVar, NVar, Fixed, M) ->
-  constraint_set:merge_and_meet(
-    normalize(PositiveEdge, [Variable | PVar], NVar, Fixed, M),
-    normalize(NegativeEdge, PVar, [Variable | NVar], Fixed, M)
-  ).
-
-all_variables(TyBDD) -> gen_bdd:all_variables(?P, TyBDD).
-
-transform({terminal, 0}, #{empty := E}) -> E();
-transform({terminal, Predef}, Ops) ->
-  ty_predef:transform(Predef, Ops);
-transform({node, Variable, PositiveEdge, NegativeEdge},
-    Ops = #{negate := Negate, var := ToVar, union := Union, intersect := Intersect}) ->
-  AstVar = ToVar(Variable),
-  Union([
-    Intersect([AstVar, transform(PositiveEdge, Ops)]),
-    Intersect([Negate(AstVar), transform(NegativeEdge, Ops)])
-  ]).
+normalize(Ty, Fixed, M) -> gen_bdd:dnf(?P, Ty, {
+  fun(Pos, Neg, Atom) -> ty_predef:normalize(Atom, Pos, Neg, Fixed, M) end,
+  fun constraint_set:meet/2
+}).
