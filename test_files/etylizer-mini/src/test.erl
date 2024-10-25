7070ee78c443aeb0aeb12e16616a531647a6befc
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2024-10-25 10:42:55 +0200

    prepared file for strange global state bug

--- test_files/etylizer-mini/src/test.erl
+++ test_files/etylizer-mini/src/test.erl
@@ -1,10 +1,9 @@
--module(varenv).
+-module(test).
 
 % @doc An environment for variables that cannot be shadowed.
 % The env is used for type variables and global functions.
 
--export([insert/3
-        ]).
+-export([insert/3]).
 
 -export_type([
     t1/2
@@ -20,10 +19,17 @@
 -spec maps_find(K, #{ K => V}) -> {ok, V} | error.
 maps_find(K, M) -> maps_find(K, M).
 
-% Inserts a new binding, an error is thrown if there already exists a binding.
+% insert/3 fails to type check with the first spec, but succeeds with the second.
+% But if we copy the whole test.erl file to the toplevel directory and the invoke
+%
+% $./ety --build --force --level debug test.erl
+%
+% type checking with the first spec works (as expected). There must be some
+% global state being involved.
 -spec insert(K, V, t1(K, V)) -> t1(K, V).
 %-spec insert(K, V, #{ K => V }) -> #{ K => V }.
 insert(Key, Val, Map) ->
+    %case maps:find(Key, Map) of
     case maps_find(Key, Map) of
         {ok, _} -> #{};
         error -> Map#{ Key => Val}
