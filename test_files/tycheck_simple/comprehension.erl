77145a3f78bc393ca78624944f6e876fd772a8cb
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2024-10-25 10:27:16 +0200

    non-active test for list comprehension #151

---
+++ test_files/tycheck_simple/comprehension.erl
@@ -0,0 +1,13 @@
+-module(maps).
+
+-compile(export_all).
+-compile(nowarn_export_all).
+
+% Each top-level definition of this module is typechecked in isolation
+% against its spec, inference is also tested.
+% If the name ends with _fail, the test must fail.
+
+% Test for a scoping bug
+% Currently deactivated because we have no support for list comprehension #151
+% -spec foo(list(T)) -> list({T, T}).
+% foo(Alts) -> [{S, A} || A <- Alts, S=A].
