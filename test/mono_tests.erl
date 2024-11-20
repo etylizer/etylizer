d2c1e82edd5a9990321e6acdcb03b026a67d2022
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2024-11-20 20:15:07 +0100

    make tests immune against variable ordering

--- test/mono_tests.erl
+++ test/mono_tests.erl
@@ -2,14 +2,22 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--spec check_mono_ty(ast:ty_scheme(), ast:ty() | cyclic) -> ok.
-check_mono_ty(Input, Expected) ->
+-spec check_mono_ty(ast:ty_scheme(), ast:ty() | [ast:ty()]| cyclic) -> ok.
+check_mono_ty(Input, cyclic) ->
     try
-        {Given, _, _} = typing_common:mono_ty(ast:loc_auto(), Input, 0),
-        ?assertEqual(Expected, Given)
+        typing_common:mono_ty(ast:loc_auto(), Input, 0),
+        ?assert(false, "expected exception")
     catch
         throw:{ety,ty_error, Msg} ->
             ?assert(utils:string_contains(Msg, "Cyclic bounds in type spec"))
+    end;
+check_mono_ty(Input, Expected) ->
+    {Given, _, _} = typing_common:mono_ty(ast:loc_auto(), Input, 0),
+    if
+        is_list(Expected) ->
+            lists:any(fun (T) -> T =:= Given end, Expected);
+        true ->
+            ?assertEqual(Expected, Given)
     end.
 
 mono_simple_test() ->
@@ -21,7 +29,7 @@ poly_simple_test() ->
     check_mono_ty({ty_scheme, [{a, Any}], {fun_full, [{var, a}], {var, a}}},
         {fun_full, [{var, 'a#0'}], {var, 'a#0'}}),
     check_mono_ty({ty_scheme, [{a, Any}, {b, Any}], {tuple, [{var, a}, {var, b}]}},
-        {tuple, [{var, 'a#0'}, {var, 'b#1'}]}).
+        [{tuple, [{var, 'a#0'}, {var, 'b#1'}]}, {tuple, [{var, 'a#1'}, {var, 'b#0'}]}]).
 
 poly_bounded_simple_test() ->
     Int = stdtypes:tint(),
