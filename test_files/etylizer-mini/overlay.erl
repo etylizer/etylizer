d35078fc258b460965326f28c0b5c66ada9a63c7
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2025-01-13 16:04:07 +0100

    progress for case study

--- test_files/etylizer-mini/overlay.erl
+++ test_files/etylizer-mini/overlay.erl
@@ -16,3 +16,19 @@
 
 -spec 'filename:join'(string(), string()) -> string().
 'filename:join'(_, _) -> error(overlay).
+
+-spec 'erlang:element'
+    (2, {_A, B}) -> B;
+    (2, {_A, B, _C}) -> B;
+    (2, {_A, B, _C, _D}) -> B;
+    (2, {_A, B, _C, _D, _E}) -> B;
+    (2, {_A, B, _C, _D, _E, _F}) -> B;
+    (2, {_A, B, _C, _D, _E, _F, _G}) -> B;
+    (2, {_A, B, _C, _D, _E, _F, _G, _H}) -> B.
+'erlang:element'(_, {_A, B}) -> B;
+'erlang:element'(_, {_A, B, _C}) -> B;
+'erlang:element'(_, {_A, B, _C, _D}) -> B;
+'erlang:element'(_, {_A, B, _C, _D, _E}) -> B;
+'erlang:element'(_, {_A, B, _C, _D, _E, _F}) -> B;
+'erlang:element'(_, {_A, B, _C, _D, _E, _F, _G}) -> B;
+'erlang:element'(_, {_A, B, _C, _D, _E, _F, _G, _H}) -> B.
