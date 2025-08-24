288e939f27030b9545b89f2c95c9ea36c6a7f0aa
albsch <38429047+albsch@users.noreply.github.com>
2025-08-24 22:18:12 +0200

    Correct scoping for F(X,X)

--- test_files/ast_transform/scope_fun.erl
+++ test_files/ast_transform/scope_fun.erl
@@ -4,6 +4,6 @@
 -compile([nowarn_export_all, nowarn_shadow_vars]).
 
 foo(X) ->
-    F = fun (X) -> X == X end,
-    G = fun F(X) -> X == F(X) end,
-    {F(X), G(X)}.
+    F = fun (X, X) -> X == X end,
+    G = fun F(X, X) -> X == F(X, X) end,
+    {F(X, X), G(X, X)}.
