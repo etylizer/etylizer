123199c5d31c2af41db0c595b34692f02bf2b997
albsch <38429047+albsch@users.noreply.github.com>
2026-01-26 00:06:59 +0100

    Refactor tally to separate satisfiability and solving

--- src/tally.erl
+++ src/tally.erl
@@ -2,95 +2,94 @@
 
 -export([
   tally/2,
+  tally/3,
   is_satisfiable/3
 ]).
 
--ifdef(TEST).
--export([
-  tally/3
-]).
--endif.
-
 -export_type([monomorphic_variables/0]).
-
 
 -type monomorphic_variables() :: term().
 -type tally_res() :: {error, [{error, string()}]} | nonempty_list(subst:t()).
 
+-spec is_satisfiable(symtab:t(), constr:collected_constrs(), sets:set(ast:ty_varname())) ->
+    {false, [{error, string()}]} | {true, term()}.
+is_satisfiable(SymTab, Constraints, FixedVars) ->
+    % uncomment to extract a tally test case config file
+    % io:format(user, "~s~n", [utils:format_tally_config(sets:to_list(Constraints), FixedVars, SymTab)]),
+
+    % erlang_types has a global symtab
+    ty_parser:set_symtab(SymTab),
+
+    Ctx = gradual_utils:new_ctx(),
+    {InlinedConstrs, _SubtyConstrs, _Maters, _UnificationSubst} = gradual_utils:preprocess_constrs(Constraints, Ctx),
+
+    InternalRawConstraints =
+    lists:map( fun ({scsubty, _, S, T}) -> {S, T} end,
+               lists:sort( fun ({scsubty, _, S, T}, {scsubty, _, X, Y}) ->
+                                   (erts_debug:size({S, T})) < erts_debug:size(({X, Y})) end,
+                           sets:to_list(InlinedConstrs))),
+
+    % cleaning is OK, we only care about one solution
+    CleanedConstraints = subst:clean_cons(InternalRawConstraints, FixedVars, SymTab),
+
+    % parse into internal representation
+    InternalConstraints = [{ty_parser:parse(T1), ty_parser:parse(T2)} || {T1, T2} <- CleanedConstraints],
+
+    % parse variables into internal representation
+    MonomorphicTallyVariables = maps:from_list([{ty_variable:new_with_name(Var), []} || Var <- sets:to_list(FixedVars)]),
+
+    % get internal result
+    InternalResult = etally:is_tally_satisfiable(InternalConstraints, MonomorphicTallyVariables),
+
+    case InternalResult of
+        false -> {false, []};
+        true -> {true, satisfiable}
+    end.
+
 -spec tally(symtab:t(), constr:collected_constrs()) -> tally_res().
 tally(SymTab, Constraints) -> tally(SymTab, Constraints, sets:new()) .
 
--spec is_satisfiable(symtab:t(), constr:collected_constrs(), sets:set(ast:ty_varname())) ->
-  {false, [{error, string()}]} | {true, subst:t()}. % The substitution is just returned for debugging purpose.
-is_satisfiable(SymTab, Constraints, FixedVars) ->
-  tally(SymTab, Constraints, FixedVars, satisfiable).
-
 -spec tally(symtab:t(), constr:collected_constrs(), sets:set(ast:ty_varname())) -> tally_res().
 tally(SymTab, Constraints, FixedVars) ->
-  tally(SymTab, Constraints, FixedVars, solve).
+    % uncomment to extract a tally test case config file
+    % io:format(user, "~s~n", [utils:format_tally_config(sets:to_list(Constraints), FixedVars, SymTab)]),
 
--spec tally
-  (symtab:t(), constr:collected_constrs(), sets:set(ast:ty_varname()), solve) -> tally_res();
-  (symtab:t(), constr:collected_constrs(), sets:set(ast:ty_varname()), satisfiable) -> {false, [{error, string()}]} | {true, subst:t()}.
-tally(SymTab, Constraints, FixedVars, Mode) ->
+    % erlang_types has a global symtab
+    ty_parser:set_symtab(SymTab),
 
-  % uncomment to extract a tally test case config file
-  % io:format(user, "~s~n", [utils:format_tally_config(sets:to_list(Constraints), FixedVars, SymTab)]),
+    Ctx = gradual_utils:new_ctx(),
+    {InlinedConstrs, SubtyConstrs, Maters, UnificationSubst} = gradual_utils:preprocess_constrs(Constraints, Ctx),
 
-  % erlang_types has a global symtab
-  ty_parser:set_symtab(SymTab),
-
-  Ctx = gradual_utils:new_ctx(),
-  {InlinedConstrs, SubtyConstrs, Maters, UnificationSubst} = gradual_utils:preprocess_constrs(Constraints, Ctx),
-
-  InternalRawConstraints =
+    InternalRawConstraints =
     lists:map( fun ({scsubty, _, S, T}) -> {S, T} end,
-      lists:sort( fun ({scsubty, _, S, T}, {scsubty, _, X, Y}) -> (erts_debug:size({S, T})) < erts_debug:size(({X, Y})) end,
-        sets:to_list(InlinedConstrs))
-    ),
+               lists:sort( fun ({scsubty, _, S, T}, {scsubty, _, X, Y}) ->
+                                   (erts_debug:size({S, T})) < erts_debug:size(({X, Y})) end,
+                           sets:to_list(InlinedConstrs))),
 
-  MaybeCleanedConstraints =
-    case Mode of
-      solve -> InternalRawConstraints;
-      satisfiable ->
-        subst:clean_cons(InternalRawConstraints, FixedVars, SymTab)
-    end,
+    InternalConstraints = [{ty_parser:parse(T1), ty_parser:parse(T2)} || {T1, T2} <- InternalRawConstraints],
 
-  InternalConstraints = [{ty_parser:parse(T1), ty_parser:parse(T2)} || {T1, T2} <- MaybeCleanedConstraints],
+    MonomorphicTallyVariables = maps:from_list([{ty_variable:new_with_name(Var), []} || Var <- sets:to_list(FixedVars)]),
 
-  MonomorphicTallyVariables = maps:from_list([{ty_variable:new_with_name(Var), []} || Var <- sets:to_list(FixedVars)]),
+    InternalResult = etally:tally(InternalConstraints, MonomorphicTallyVariables),
 
-  case Mode of
-    solve ->
-      InternalResult = etally:tally(InternalConstraints, MonomorphicTallyVariables),
+    Free = tyutils:free_in_subty_constrs(InlinedConstrs),
+    case InternalResult of
+        {error, []} -> {error, []};
+        _ ->
+            % transform to subst:t()
+            Sigmas = [subst:mk_tally_subst(
+               sets:union(FixedVars, Free),
+               maps:from_list([{VarName, ty_parser:unparse(Ty)}
+                               || {{var, _, VarName, _}, Ty} <- maps:to_list(Subst)])) % FIXME depends on internal ty_variable representation
+             || Subst <- InternalResult],
 
-      Free = tyutils:free_in_subty_constrs(InlinedConstrs),
-      case InternalResult of
-            {error, []} ->
-              {error, []};
-            _ ->
-              % transform to subst:t()
-              Sigmas = [subst:mk_tally_subst(
-                sets:union(FixedVars, Free),
-                maps:from_list([{VarName, ty_parser:unparse(Ty)}
-                              || {{var, _, VarName, _}, Ty} <- maps:to_list(Subst)])) % FIXME depends on internal ty_variable representation
-              || Subst <- InternalResult],
-
-              lists:map(
-                fun({tally_subst, S, Fixed}) ->
-                  MaterSubst = maps:fold(fun(Var, Ty, MAcc) ->
-                      maps:put(Var, subst:apply_base(S, Ty), MAcc)
-                    end,
-                    #{}, UnificationSubst),
-                  gradual_utils:postprocess({tally_subst, maps:merge(S, MaterSubst), Fixed}, SubtyConstrs, Maters, SymTab)
-                end,
-                Sigmas)
-      end;
-    satisfiable ->
-      InternalResult = etally:is_tally_satisfiable(InternalConstraints, MonomorphicTallyVariables),
-
-      case InternalResult of
-        false -> {false, []};
-        true -> {true, subst:empty()}
-      end
-  end.
+            lists:map(
+              fun({tally_subst, S, Fixed}) ->
+                MaterSubst = maps:fold(fun(Var, Ty, MAcc) ->
+                    maps:put(Var, subst:apply_base(S, Ty), MAcc)
+                  end,
+                  #{}, UnificationSubst),
+                gradual_utils:postprocess({tally_subst, maps:merge(S, MaterSubst), Fixed}, SubtyConstrs, Maters, SymTab)
+              end,
+              Sigmas)
+      end.
