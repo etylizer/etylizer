a7652613d608f26653a13f6824f006b73c74856e
albsch <38429047+albsch@users.noreply.github.com>
2026-01-29 00:52:47 +0100

    [{etally,global_state}.erl] type safe

--- src/erlang_types/global_state.erl
+++ src/erlang_types/global_state.erl
@@ -8,19 +8,20 @@
 
 -callback init() -> _.
 
--spec modules_with_global_state() -> [module()].
-modules_with_global_state() -> [ty_node, ty_parser, ty_variable].
-
 -spec init() -> _.
 init() ->
-  lists:foreach(fun(M) -> M:init() end, modules_with_global_state()).
+    ty_node:init(),
+    ty_parser:init(),
+    ty_variable:init().
 
 -spec clean() -> _.
 clean() ->
-  lists:foreach(fun(M) -> M:clean() end, modules_with_global_state()).
+    ty_node:clean(),
+    ty_parser:clean(),
+    ty_variable:clean().
 
 -spec with_new_state(fun(() -> _)) -> _.
 with_new_state(Fun) ->
-  clean(),
-  init(),
-  Fun().
+    clean(),
+    init(),
+    Fun().
