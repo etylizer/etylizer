b22842423bbd62c84ea901598c4c0dabf126a220
albsch <38429047+albsch@users.noreply.github.com>
2026-03-08 01:54:15 +0100

    Exhaustiveness heuristic#280

--- src/typing.hrl
+++ src/typing.hrl
@@ -5,7 +5,10 @@
           gradual_typing_mode = dynamic :: feature_flags:gradual_typing_mode(),
           report_mode :: feature_flags:report_mode(),
           report_timeout :: pos_integer(),
-          exhaustiveness_mode :: feature_flags:exhaustiveness_mode()
+          exhaustiveness_mode :: feature_flags:exhaustiveness_mode(),
+          % functions where exhaustiveness is disabled at the constraint generation level
+          % via a -disable_exhaustiveness user-specified attribute
+          disable_exhaustiveness = sets:new() :: sets:set({atom(), arity()})
         }).
 
 -type ctx() :: #ctx{}.
